c3f2a6ac4b17bc0d42e73863f6f31941cb473942
Enerelt Bat-Erdene <enerelt2009@yahoo.co.uk>
2020-12-17 17:52:40 -0800

    adding port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
 	console.log(`app runs on ${PORT}`);
 })
 
